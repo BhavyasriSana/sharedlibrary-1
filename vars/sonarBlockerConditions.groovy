7b76a6f6945b5e0510504726e843964d99ab223c
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 16:35:09 +0530

    Update sonarBlockerConditions.groovy

--- vars/sonarBlockerConditions.groovy
+++ vars/sonarBlockerConditions.groovy
@@ -4,30 +4,11 @@ import groovy.json.JsonSlurper
 createRepo(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
-def GateId = resultJson.gateid
-def credentials = resultJson.cname
-//def rid = resultJson.id
-//def projUrl = resultJson.url
+def GateId = resultJson.id
 
-httpRequest authentication: "${credentials}", contentType: "APPLICATION_JSON", 
-    
-    httpMode: 'POST',/* requestBody: 
-  """{
-    	"data":
-	{
-		"repoType": "hosted",
-        "id": ${rid},
-        "name": ${repoName},
-        "repoPolicy": "RELEASE",
-        "provider": "maven2",
-        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
-        "exposed": true,
-        "format": "maven2"
-	}
-        
-   }""",*/ url: "http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=${GateId}&metric=blocker_violations&op=GT&warning=5&error=10"
-}
+	sh "curl --location --request POST 'http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=${GateId}&metric=blocker_violations&op=GT&warning=5&error=10' \
+--header 'Authorization: Basic YWRtaW46YWRtaW4='"
 	def call(){
-def request = libraryResource 'sonarConnectorData.json'
+def request = libraryResource 'QualityGateDetails.json'
 createRepo(request)
 }
