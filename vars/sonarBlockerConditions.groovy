93d5a8bed1576eb2b19613cc1b5ec92a8b5efe20
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 17:37:14 +0530

    Update sonarBlockerConditions.groovy

--- vars/sonarBlockerConditions.groovy
+++ vars/sonarBlockerConditions.groovy
@@ -1,15 +1,13 @@
 import groovy.json.JsonSlurper 
 
 @NonCPS
-createRepo(String data){
-def jsonSlurper = new JsonSlurper() 
-def resultJson = jsonSlurper.parseText(data)
+def call(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/sonar/QualityGateDetails.json"),"UTF-8"))
+def resultJson = jsonSlurper.parseText(reader)
 def GateId = resultJson.id
 
 	sh "curl --location --request POST 'http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=${GateId}&metric=blocker_violations&op=GT&warning=5&error=10' \
 --header 'Authorization: Basic YWRtaW46YWRtaW4='"
 }
-	def call(){
-def request = Workspace 'QualityGateDetails.json'
-createRepo(request)
-}
+
