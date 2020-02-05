7a589aceb49b97054453c6d89e9b899430be92f6
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 16:07:23 +0530

    Update sonarProject.groovy

--- vars/sonarProject.groovy
+++ vars/sonarProject.groovy
@@ -1,6 +1,6 @@
 def call(jsondata){
 def jsonString = jsondata
-//println(jsonString)
+println(jsonString)
 def jsonObj = readJSON text: jsonString
 println(jsonObj.code_quality)
 
@@ -9,7 +9,7 @@ String ProjectKey=a.replaceAll("\\[", "").replaceAll("\\]","");
 	
 String b = jsonObj.code_quality.projects.project.project_name
 String ProjectName=b.replaceAll("\\[", "").replaceAll("\\]","");
-//def credentials = resultJson.cname
+	
 	sh "curl --location --request POST 'http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}' \
 --header 'Authorization: Basic YWRtaW46YWRtaW4='"
 }
