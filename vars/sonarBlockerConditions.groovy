5186bf16d8849042d456308e296468e157b95940
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 16:41:26 +0530

    Update sonarBlockerConditions.groovy

--- vars/sonarBlockerConditions.groovy
+++ vars/sonarBlockerConditions.groovy
@@ -8,6 +8,7 @@ def GateId = resultJson.id
 
 	sh "curl --location --request POST 'http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=${GateId}&metric=blocker_violations&op=GT&warning=5&error=10' \
 --header 'Authorization: Basic YWRtaW46YWRtaW4='"
+}
 	def call(){
 def request = libraryResource 'QualityGateDetails.json'
 createRepo(request)
