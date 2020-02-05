784a049ff81bb856cf046d71a130b8afc778336d
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 12:57:11 +0530

    Update sonarProject.groovy

--- vars/sonarProject.groovy
+++ vars/sonarProject.groovy
@@ -4,9 +4,8 @@ import groovy.json.JsonSlurper
 createRepo(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
-def ProjectKey = resultJson.pkey
-def ProjectName = resultJson.pname
-def QualityGateName = resultJson.qname
+def ProjectKey = resultJson.code_quality.projects.project.project_key
+def ProjectName = resultJson.code_quality.projects.project.project_name
 def credentials = resultJson.cname
 	sh "curl --location --request POST 'http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}' \
 --header 'Authorization: Basic YWRtaW46YWRtaW4='"
