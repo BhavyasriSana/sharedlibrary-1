3c2b1ad07da06569f5e8e1004e2734ebf18863bb
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 13:42:31 +0530

    Update sonarQualityGate.groovy

--- vars/sonarQualityGate.groovy
+++ vars/sonarQualityGate.groovy
@@ -1,32 +1,12 @@
-import groovy.json.JsonSlurper 
+def call(jsondata){
+def jsonString = jsondata
+println(jsonString)
+def jsonObj = readJSON text: jsonString
+//println(jsonObj.code_quality)
 
-@NonCPS
-createRepo(String data){
-def jsonSlurper = new JsonSlurper() 
-def resultJson = jsonSlurper.parseText(data)
-def QualityGateName = resultJson.qname
-def credentials = resultJson.cname
-//def projUrl = resultJson.url
+String a = jsonObj.code_quality.projects.project.quality_gate.gate_name
+String QualityGateName=a.replaceAll("\\[", "").replaceAll("\\]","");
 
-httpRequest authentication: "${credentials}", contentType: "APPLICATION_JSON", 
-    
-    httpMode: 'POST',/* requestBody: 
-  """{
-    	"data":
-	{
-		"repoType": "hosted",
-        "id": ${rid},
-        "name": ${repoName},
-        "repoPolicy": "RELEASE",
-        "provider": "maven2",
-        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
-        "exposed": true,
-        "format": "maven2"
-	}
-        
-   }""",*/ url: "http://3.16.33.107:9000/api/qualitygates/create?name=${QualityGateName}"
-}
-	def call(){
-def request = libraryResource 'sonarConnectorData.json'
-createRepo(request)
+	sh "curl --location --request POST 'http://3.16.33.107:9000/api/qualitygates/create?name=${QualityGateName}' \
+--header 'Authorization: Basic YWRtaW46YWRtaW4='"
 }
