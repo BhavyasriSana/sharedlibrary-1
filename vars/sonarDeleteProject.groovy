cba64e8fa94aa355694366de1d441db2d18665ea
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 19:31:02 +0530

    Update sonarDeleteProject.groovy

--- vars/sonarDeleteProject.groovy
+++ vars/sonarDeleteProject.groovy
@@ -7,6 +7,7 @@ def jsonObj = readJSON text: jsonString
 String a = jsonObj.code_quality.projects.project.project_key
 String ProjectKey=a.replaceAll("\\[", "").replaceAll("\\]","");
 	
-	"http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/delete?key=${delkey}"
+
 	sh "curl --location --request POST 'http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/delete?key=${ProjectKey}' \
 --header 'Authorization: Basic YWRtaW46YWRtaW4='"
+}
