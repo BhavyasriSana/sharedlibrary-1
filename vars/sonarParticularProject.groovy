0e27be6b366d94c587edd8057cc70c7f92f1d05f
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 15:58:59 +0530

    Update sonarParticularProject.groovy

--- vars/sonarParticularProject.groovy
+++ vars/sonarParticularProject.groovy
@@ -9,5 +9,5 @@ String ProjectKey=a.replaceAll("\\[", "").replaceAll("\\]","");
 
 
 	sh "curl --location --request GET 'http://3.16.33.107:9000/api/measures/component?metricKeys=ncloc,complexity,violations&component=${ProjectKey}' \
---header 'Authorization: Basic YWRtaW46YWRtaW4=' -o ProjectDetails2.json"
+--header 'Authorization: Basic YWRtaW46YWRtaW4='"
 }
