0e2fad832df926c63d82267abefefc0e8c2669f6
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 12:38:41 +0530

    Update sonarProject.groovy

--- vars/sonarProject.groovy
+++ vars/sonarProject.groovy
@@ -8,8 +8,8 @@ def ProjectKey = resultJson.pkey
 def ProjectName = resultJson.pname
 def QualityGateName = resultJson.qname
 def credentials = resultJson.cname
-	"""sh curl --location --request POST 'http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}' \
---header 'Authorization: Basic YWRtaW46YWRtaW4='"""
+	sh "curl --location --request POST 'http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}' \
+--header 'Authorization: Basic YWRtaW46YWRtaW4='"
 }
 	def call(){
 def request = libraryResource 'sonarConnectorData.json'
