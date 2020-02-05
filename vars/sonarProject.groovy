ec8852bca7fa5a538cf8e55c70bcd9dbf1cf9306
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 12:15:33 +0530

    Update sonarProject.groovy

--- vars/sonarProject.groovy
+++ vars/sonarProject.groovy
@@ -9,8 +9,10 @@ def ProjectName = resultJson.pname
 def QualityGateName = resultJson.qname
 def credentials = resultJson.cname
 //def projUrl = resultJson.url
+	"""sh curl --location --request POST 'http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}' \
+--header 'Authorization: Basic YWRtaW46YWRtaW4='"""
 
-httpRequest authentication: "${credentials}", contentType: "APPLICATION_JSON", 
+/*httpRequest authentication: "${credentials}", contentType: "APPLICATION_JSON", 
     
     httpMode: 'POST',/* requestBody: 
   """{
@@ -26,7 +28,7 @@ httpRequest authentication: "${credentials}", contentType: "APPLICATION_JSON",
         "format": "maven2"
 	}
         
-   }""",*/ url: "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}"
+   }""",*//* url: "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}"*/
 	
 }
 	def call(){
