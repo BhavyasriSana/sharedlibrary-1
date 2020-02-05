d0f4a49e661694d903befc8b2ab9187242ed1eca
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 18:19:26 +0530

    Update sonarDeleteProject.groovy

--- vars/sonarDeleteProject.groovy
+++ vars/sonarDeleteProject.groovy
@@ -1,33 +1,12 @@
-import groovy.json.JsonSlurper 
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+//println(jsonObj.code_quality)
 
-@NonCPS
-createRepo(String data){
-def jsonSlurper = new JsonSlurper() 
-def resultJson = jsonSlurper.parseText(data)
-def credentials = resultJson.cname
-def delkey = resultJson.delkey
-//def projUrl = resultJson.url
-
-httpRequest authentication: "${credentials}", contentType: "APPLICATION_JSON", 
-    
-    httpMode: 'POST',/* requestBody: 
-  """{
-    	"data":
-	{
-		"repoType": "hosted",
-        "id": ${rid},
-        "name": ${repoName},
-        "repoPolicy": "RELEASE",
-        "provider": "maven2",
-        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
-        "exposed": true,
-        "format": "maven2"
-	}
-        
-   }""",*/ url: "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/delete?key=${delkey}"
+String a = jsonObj.code_quality.projects.project.project_key
+String ProjectKey=a.replaceAll("\\[", "").replaceAll("\\]","");
 	
-}
-	def call(){
-def request = libraryResource 'sonarConnectorData.json'
-createRepo(request)
-}
+	"http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/delete?key=${delkey}"
+	sh "curl --location --request POST 'http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/delete?key=${ProjectKey}' \
+--header 'Authorization: Basic YWRtaW46YWRtaW4='"
