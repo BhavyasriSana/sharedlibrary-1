7d20e62cb6c490696e3130fd24dc6a143a0b9ead
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-05 19:51:49 +0530

    Update sonarDeleteQualityGate.groovy

--- vars/sonarDeleteQualityGate.groovy
+++ vars/sonarDeleteQualityGate.groovy
@@ -7,7 +7,7 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def GateId = resultJson.id
 
-	sh "curl --location --request POST 'http://3.16.33.107:9000/api/projects/delete?key=${GateId}' \
+	sh "curl --location --request POST 'http://3.16.33.107:9000/api/qualitygates/destroy?id=${GateId}' \
 --header 'Authorization: Basic YWRtaW46YWRtaW4='"
 }
 def call(){
