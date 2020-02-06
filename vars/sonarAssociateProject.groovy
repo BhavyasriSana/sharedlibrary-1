a7744b2215e0ae1d798c15053d9df629706830dc
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-06 13:34:32 +0530

    Update sonarAssociateProject.groovy

--- vars/sonarAssociateProject.groovy
+++ vars/sonarAssociateProject.groovy
@@ -9,7 +9,7 @@ def reader2 = new BufferedReader(new InputStreamReader(new FileInputStream("/var
 def resultJson = jsonSlurper.parse(reader)
 def resultJson2 = jsonSlurper.parse(reader2)
 def GateId = resultJson.id
-def ProjectId = resultJson.component.id
+def ProjectId = resultJson2.component.id
 	sh "curl --location --request POST 'http://3.16.33.107:9000/api/qualitygates/select?gateId=${GateId}&projectId=${ProjectId}' \
 --header 'Authorization: Basic YWRtaW46YWRtaW4='"
 }
