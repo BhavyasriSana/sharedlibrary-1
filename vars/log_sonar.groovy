b73ffa99aa0071ccfc197ed20e32643a73d02998
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-06 20:06:23 +0530

    Update log_sonar.groovy

--- vars/log_sonar.groovy
+++ vars/log_sonar.groovy
@@ -1,4 +1,4 @@
-import groovy.json.JsonSlurper 
+/*import groovy.json.JsonSlurper 
 @NonCPS
 
 def call(message)
@@ -9,4 +9,19 @@ def call(message)
  def resultJson = jsonSlurper.parseText(request)
  def rpkey = resultJson.pkey
    sh " echo '${rpkey}' ${message} >>log.txt"
+}*/
+
+def call(message,jsondata)
+{
+  
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+println(jsonObj.code_quality)
+
+String a=jsonObj.code_quality.projects.project.project_name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+  
+ println(message)
+  Date date = new Date() 
+  sh " echo '${date}' SONAR '${projectName}' ${message} >>log.txt"
 }
