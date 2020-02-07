d40be9089b645d6db9d42c19d13318e463b5ff67
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-07 12:14:40 +0530

    Update sonarDeleteQualityGate.groovy

--- vars/sonarDeleteQualityGate.groovy
+++ vars/sonarDeleteQualityGate.groovy
@@ -3,7 +3,7 @@ import groovy.json.*
 @NonCPS
 create(){
 def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/sonar/QualityGateDetails.json"),"UTF-8"))
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/QualityGateDetails.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 def GateId = resultJson.id
 
