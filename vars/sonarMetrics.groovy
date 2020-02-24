f86896de9ee69389eb44a193d34067d2827f1a90
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-24 12:27:40 +0530

    Update sonarMetrics.groovy

--- vars/sonarMetrics.groovy
+++ vars/sonarMetrics.groovy
@@ -1,6 +1,10 @@
 def call(){
 withCredentials([usernamePassword(credentialsId: 'sonar_cred', passwordVariable: 'password', usernameVariable: 'username')]){
-  sh "curl -u ${username}:${password} -X GET 'http://ec2-3-133-107-212.us-east-2.compute.amazonaws.com:9000/api/measures/component?component=BMIBeta&metricKeys=coverage,vulnerabilities,bugs,violations,complexity' -o metrics.json"
+  """curl -0 -v -u ${username}:${password} -X GET http://ec2-3-133-107-212.us-east-2.compute.amazonaws.com:9000/api/measures/component?component=BMIBeta&metricKeys=coverage,vulnerabilities,bugs,violations,complexity \
+  -H "Expect:" \
+  -H 'Content-Type: text/json; charset=utf-8' \
+  -d @myfile.json"""
+  //sh "curl -u ${username}:${password} -X GET 'http://ec2-3-133-107-212.us-east-2.compute.amazonaws.com:9000/api/measures/component?component=BMIBeta&metricKeys=coverage,vulnerabilities,bugs,violations,complexity' -o metrics.json"
   echo 'metrics collected'
 }
 }
