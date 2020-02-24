389784bad094a6919d1f977e806ad071ab16b8a1
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-02-24 11:22:21 +0530

    Update sonarMetricVulnerabilities.groovy

--- vars/sonarMetricVulnerabilities.groovy
+++ vars/sonarMetricVulnerabilities.groovy
@@ -1,4 +1,5 @@
 def call(){
-withCredentials([usernamePassword(credentialsId: 'sonar_cred', passwordVariable: 'password', usernameVariable: 'username')])
+withCredentials([usernamePassword(credentialsId: 'sonar_cred', passwordVariable: 'password', usernameVariable: 'username')]){
 sh 'curl -u username:password -X GET http://ec2-3-133-107-212.us-east-2.compute.amazonaws.com:9000/api/measures/component?component=BMIBeta&metricKeys=vulnerabilities'
+}
 }
