1e386d8777de613d8530cbf7ad4529723168e9be
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-06 15:24:49 +0530

    Update sonarMetrics.groovy

--- vars/sonarMetrics.groovy
+++ vars/sonarMetrics.groovy
@@ -1,3 +1,69 @@
+import groovy.json.*
+
+@NonCPS
+create(){
+  List<String> LIST = new ArrayList<String>();
+  def jsonObj = readJSON text: metrics
+  int score=10;
+  for(i=0;i<jsonObj.component.measures.size();i++){
+    def metric=jsonObj.component.measures[i].metric
+    def d=jsonObj.component.measures[i].value
+    int data = Integer.parseInt(d)
+    
+    if(metric.equals("sqale_index")){
+      if(data<10){
+        score+=10;
+        LIST.add(["metric":metric,"score":score])
+      }
+    }
+    if(metric.equals("vulnerabilities")){
+      if(data<10){
+        score+=10;
+        LIST.add(["metric":metric,"score":score])
+      }
+    }
+    if(metric.equals("coverage")){
+      if(data>20){
+        score+=10;
+        LIST.add(["metric":metric,"score":score])
+      }
+    }
+    if(metric.equals("duplicated_lines")){
+      if(data<100){
+        score+=10;
+        LIST.add(["metric":metric,"score":score])
+      }
+    }
+    if(metric.equals("complexity")){
+      if(data<10){
+        score+=10;
+        LIST.add(["metric":metric,"score":score])
+      }
+    }
+    if(metric.equals("violations")){
+      if(data<10){
+        score+=10;
+        LIST.add(["metric":metric,"score":score])
+      }
+    }
+    if(metric.equals("bugs")){
+      if(data<10){
+        score+=10;
+        LIST.add(["metric":metric,"score":score])
+      }
+    }
+    if(metric.equals("tests")){
+      if(data<10){
+        score+=10;
+        LIST.add(["metric":metric,"score":score])
+      }
+    }
+  }
+  print(List)
+  
+
+}
+
 def call(jsondata){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
@@ -8,5 +74,8 @@ String ProjectName=a.replaceAll("\\[", "").replaceAll("\\]","");
 withCredentials([usernamePassword(credentialsId: 'sonar_cred', passwordVariable: 'password', usernameVariable: 'username')]){
   sh "curl -u ${username}:${password} -X GET 'http://ec2-3-133-107-212.us-east-2.compute.amazonaws.com:9000/api/measures/component?component=${ProjectName}&metricKeys=coverage,vulnerabilities,bugs,violations,complexity,tests,duplicated_lines,sqale_index' -o metrics.json"
   echo 'metrics collected'
+}
+  def call(){
+create()
 }
 }
