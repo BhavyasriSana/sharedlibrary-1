1fa857695287a5fd621a66e0522490cc982b92f6
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-04-28 16:01:32 +0530

    Delete sonarSetAsDefault.groovy

--- sonarSetAsDefault.groovy
+++
@@ -1,3 +0,0 @@
-http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/qualitygates/set_as_default?id=57
-  
-  http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/qualitygates/rename?id=59&name=QG2
