90931420cf417f9c86f65ac49213087244ca7ee7
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-25 00:40:33 +0530

    Update influx.groovy

--- vars/influx.groovy
+++ vars/influx.groovy
@@ -5,7 +5,8 @@ create(){
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
-def total = resultJson.component.measures[1].value
+//def total = resultJson.component.measures[1].value
+  def total = 10
   echo "$total"
 sh """curl -i -XPOST 'http://18.222.223.64/write?db=mydb' --data-binary 'complexity =${total}' 
 """
