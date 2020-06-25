571286e4aba05568fb36257c98e44f0cd2748b66
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-25 12:35:39 +0530

    Update influx.groovy

--- vars/influx.groovy
+++ vars/influx.groovy
@@ -6,11 +6,11 @@ def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
   print (resultJson)
-  def total=resultJson.Sonar.Metrics.component.measures[1].metric
+  def total=resultJson.Sonar.Metrics.component.measures[1].value
   //def total = 10
   echo "$total"
 //sh """curl -k --connect-timeout 30 -XPOST "http://18.222.223.64:8086/write?db=mydb&precision=s" --data-binary '${total}'"""
-  sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary '${total}'"""
+  sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'sonar complexity=${total}'"""
 }
 
 
