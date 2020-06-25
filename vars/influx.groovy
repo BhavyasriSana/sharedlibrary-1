e0f8b02337fa6752375cdef39f1a6668df863084
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-25 17:46:43 +0530

    Update influx.groovy

--- vars/influx.groovy
+++ vars/influx.groovy
@@ -6,11 +6,13 @@ def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
   print (resultJson)
-  def total=resultJson.Sonar.Metrics.component.measures[1].value
+  def t0=resultJson.Sonar.Metrics.component.measures[0].value
+  def t1=resultJson.Sonar.Metrics.component.measures[1].value
+  def t2=resultJson.Sonar.Metrics.component.measures[2].value
+  def t3=resultJson.Sonar.Metrics.component.measures[3].value
   //def total = 10
-  echo "$total"
 //sh """curl -k --connect-timeout 30 -XPOST "http://18.222.223.64:8086/write?db=mydb&precision=s" --data-binary '${total}'"""
-  sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-raw @/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"""
+  sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONAR,duplicated_lines=${t0} complexity=${t1} violations=${t2} sqale_index=${t3}'"""
 }
 
 
