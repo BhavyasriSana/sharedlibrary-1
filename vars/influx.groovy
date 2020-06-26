caac2afeaf8c0dfecdb517ac0f98abc6e1f15532
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-26 13:58:26 +0530

    Update influx.groovy

--- vars/influx.groovy
+++ vars/influx.groovy
@@ -13,8 +13,8 @@ def resultJson = jsonSlurper.parse(reader)
   //def total = 10
 //sh """curl -k --connect-timeout 30 -XPOST "http://18.222.223.64:8086/write?db=mydb&precision=s" --data-binary '${total}'"""
   //sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARDATA,Metric=duplicated_lines Value=${t0}'"""
- // sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARDATA,Metric=complexity Value=${t1}'"""
-  sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARDATA,Metric=violations Value=${t2}'"""
+  sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARDATA,Metric=complexity Value=${t1}'"""
+  //sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARDATA,Metric=violations Value=${t2}'"""
   //sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARDATA,Metric=sqale_index Value=${t3}'"""
 }
 
