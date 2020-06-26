92e1d41e1de9c3d37f7c29f4ee11f98584f71553
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-26 13:39:02 +0530

    Update influx.groovy

--- vars/influx.groovy
+++ vars/influx.groovy
@@ -12,10 +12,10 @@ def resultJson = jsonSlurper.parse(reader)
   def t3=resultJson.Sonar.Metrics.component.measures[3].value
   //def total = 10
 //sh """curl -k --connect-timeout 30 -XPOST "http://18.222.223.64:8086/write?db=mydb&precision=s" --data-binary '${total}'"""
-  sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARQUB,Metric=duplicated_lines Value=${t0} 1463689152000000000
-  SONARQUB,Metric=complexity Value=${t1} 1463689152000000000
-  SONARQUB,Metric=violations Value=${t2} 1463689152000000000
-  SONARQUB,Metric=sqale_index Value=${t3} 1463689152000000000'"""
+  sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARQUB,Metric=duplicated_lines Value=${t0}
+  SONARQUB,Metric=complexity Value=${t1}
+  SONARQUB,Metric=violations Value=${t2}
+  SONARQUB,Metric=sqale_index Value=${t3}'"""
 }
 
 
