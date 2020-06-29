16afeb97862c7095388a1658ee6f09256919dbc1
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-29 22:02:45 +0530

    Update gitInflux.groovy

--- vars/gitInflux.groovy
+++ vars/gitInflux.groovy
@@ -6,7 +6,7 @@ create(){
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/gitrepos.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
-  print (resultJson)
+  //print (resultJson)
 def repocount = resultJson.size()
 //sh """curl -k --connect-timeout 30 -XPOST "http://18.222.223.64:8086/write?db=mydb&precision=s" --data-binary '${total}'"""
   sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'Git,Metric=GitRepoCount Value=${repocount} 1593159883918987231'"""
