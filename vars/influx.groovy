e46ba53a0a941cc7f097f5e6ee84acfd9ddeae60
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-29 18:30:57 +0530

    Update influx.groovy

--- vars/influx.groovy
+++ vars/influx.groovy
@@ -23,5 +23,22 @@ def resultJson = jsonSlurper.parse(reader)
 
 def call()
 {
+	def jsonSlurper = new JsonSlurper()
+	def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"),"UTF-8"))
+	def resultJson = jsonSlurper.parse(reader)
+  	print (resultJson)
+  	def size=resultJson.Sonar.Metrics.component.measures.size
+  	print(size)
+  	for(int i=0;i<size;i++){
+		
+    		print(i)
+    	  	String t0=resultJson.Sonar.Metrics.component.measures[i].metric
+	  	String metric=t0.replaceAll("\\[", "").replaceAll("\\]","");
+	  	String t3=resultJson.Sonar.Metrics.component.measures[i].value
+		int value = Integer.parseInt(t1);
+		print (metric)
+          	print (value)
+	}
+
 	create()
 }
