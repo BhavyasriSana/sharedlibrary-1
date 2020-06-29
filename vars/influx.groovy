b9610966872e8b305076fc105ed28834d46b1978
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-29 14:11:01 +0530

    Update influx.groovy

--- vars/influx.groovy
+++ vars/influx.groovy
@@ -1,7 +1,7 @@
 import groovy.json.*
 
 @NonCPS
-create(String t0,object t1){
+create(String t0,Object t1){
       sh "curl -i -XPOST http://18.222.223.64:8086/write?db=SonarDB --data-binary 'SONARMETRIC,Metric=${t0} Value=${t1}'"
 }
 def call(){
