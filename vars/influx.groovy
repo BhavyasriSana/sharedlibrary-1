ae7b4d8319f689f1e3764df2a433acc48268dd3c
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-29 15:54:49 +0530

    Update influx.groovy

--- vars/influx.groovy
+++ vars/influx.groovy
@@ -1,35 +1,8 @@
 import groovy.json.*
 
-@NonCPS
-create(String t0,int t1){
-      sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=SonarDB" --data-binary 'SONARMETRIC,Metric=${t0} Value=${t1}'"""
-}
-def call(){
-def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
-  print (resultJson)
-  def size=resultJson.Sonar.Metrics.component.measures.size
-  print(size)
-  for(int i=0;i<size;i++){
-    print(i)
-    String t0=resultJson.Sonar.Metrics.component.measures[i].metric
-	  String metric=t0.replaceAll("\\[", "").replaceAll("\\]","");
-    int t1=Integer.parseInt(resultJson.Sonar.Metrics.component.measures[i].value)
-	  //int value = Integer.parseInt(t1);
-    	  print (t0)
-          print (t1)
- 	  create(metric,t1)
-  }
-
-}
-/*import groovy.json.*
 
 @NonCPS
-create(String t0,int t1){
-  sh "curl -i -XPOST http://18.222.223.64:8086/write?db=SonarDB --data-binary 'SONARMETRIC,Metric=${t0} Value=${t1}'"
-}
-/*create(){
+create(){
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
@@ -44,26 +17,10 @@ def resultJson = jsonSlurper.parse(reader)
   sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARDATA,Metric=complexity type=intrgral server=sonar Value=${t1} 1593159883918987232'"""
   sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARDATA,Metric=violations type=intrgral server=sonar Value=${t2} 1593159883918987233'"""
   sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARDATA,Metric=sqale_index type=intrgral server=sonar Value=${t3} 1593159883918987234'"""
-}*/
+}
 
 
-/*def call()
+def call()
 {
-  def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
-  print (resultJson)
-  def size=resultJson.Sonar.Metrics.component.measures.size
-  print(size)
-  for(int i=0;i<size;i++){
-    print(i)
-    def t0=resultJson.Sonar.Metrics.component.measures[i].metric
-    def t1=resultJson.Sonar.Metrics.component.measures[i].value
-    print (t0)
-    print (t1)
-    create(t0,t1)
-    //sh "curl -i -XPOST http://18.222.223.64:8086/write?db=SonarDB --data-binary 'SONARMETRIC,Metric=${t0} Value=${t1}'"
-    
-    print(i)
-  }
-}*/
+	create()
+}
