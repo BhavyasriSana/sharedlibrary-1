7631b2b8c1531f7f5fab1f6f02dd9095e635d232
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-29 18:06:48 +0530

    Update SonarInflux.groovy

--- vars/SonarInflux.groovy
+++ vars/SonarInflux.groovy
@@ -2,6 +2,44 @@ import groovy.json.*
 
 @NonCPS
 create(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/commit.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def count = resultJson.count
+echo "$count"
+	def t0=1
+	def t1=2
+      sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=SonarDB" --data-binary 'SONARMETRIC,Metric=${t0} Value=${t1}'"""
+}
+
+
+def call()
+{
+	def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+  print (resultJson)
+  def size=resultJson.Sonar.Metrics.component.measures.size
+  print(size)
+  for(int i=0;i<size;i++){
+    print(i)
+    	  String t0=resultJson.Sonar.Metrics.component.measures[i].metric
+	  String metric=t0.replaceAll("\\[", "").replaceAll("\\]","");
+	  String t3=resultJson.Sonar.Metrics.component.measures[i].value
+	  
+    //int t1=Integer.parseInt(resultJson.Sonar.Metrics.component.measures[i].value)
+	  //int value = Integer.parseInt(t1);
+    	  print (t0)
+          print (t3)
+ 	  //create(metric,t3)
+	  
+  }
+create()
+}
+/*import groovy.json.*
+
+@NonCPS
+create(){
 //create(String t0,String t1){
 	def t0=1
 	def t1=2
@@ -29,3 +67,4 @@ def resultJson = jsonSlurper.parse(reader)
   }
 	create()
 }
+*/
