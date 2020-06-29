a8c2f4a3fddd31d6606c3ed62214ac5ac72aab5a
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-29 22:01:56 +0530

    Update gitInflux.groovy

--- vars/gitInflux.groovy
+++ vars/gitInflux.groovy
@@ -4,7 +4,7 @@ import groovy.json.*
 @NonCPS
 create(){
 def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"),"UTF-8"))
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/gitrepos.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
   print (resultJson)
 def repocount = resultJson.size()
