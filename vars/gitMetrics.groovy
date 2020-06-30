25fe932b96dee2ed4d34e8be8570a5fd309e8f7f
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-30 15:49:29 +0530

    Update gitMetrics.groovy

--- vars/gitMetrics.groovy
+++ vars/gitMetrics.groovy
@@ -1,12 +1,12 @@
 import groovy.json.*
 
-def call(){
+def call(JSON){
 	def jsonObja = readJSON text: JSON
 	def IP=jsonObja.GIT.GitUrl
   print(IP)
 	def user=jsonObja.GIT.GitUserName
 	def pass=jsonObja.GIT.GitPassword
   def repoName=jsonObja.GIT.GitRepoName
- sh 'curl -v -u $user:$pass -H "ContentType: application/json; charset=UTF-8"  ${IP}/users/${user}/repos -o gitrepos.json'
+ sh "curl -u $user:$pass -H "ContentType: application/json; charset=UTF-8"  ${IP}/users/${user}/repos -o gitrepos.json"
  sh "curl -X GET -u $user:$pass ${IP}/repos/${user}/${repoName}/commits -o commits.json"
 }
