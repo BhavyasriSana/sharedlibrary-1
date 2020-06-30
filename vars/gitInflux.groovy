b68ca97efb6ce727d21572264e61ee3357e646da
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-30 16:38:15 +0530

    Update gitInflux.groovy

--- vars/gitInflux.groovy
+++ vars/gitInflux.groovy
@@ -9,6 +9,14 @@ def resultJson = jsonSlurper.parse(reader)
   //print (resultJson)
 def repocount = resultJson.size()
   sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'Git,Metric=GitRepoCount Value=${repocount}'"""
+	
+def jsonSlurper2 = new JsonSlurper()
+def reader2 = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/commits.json"),"UTF-8"))
+def resultJson2 = jsonSlurper.parse(reader2)
+  //print (resultJson2)
+def commitscount = resultJson2.size()
+	sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'Git,Metric=GitCommitsCountInRepo Value=${commitscount}'"""
+	
 }
 
 
