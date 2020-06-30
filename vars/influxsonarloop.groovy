64c2da3d8349585225345cc3831aaca0f764e4bb
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-30 12:25:25 +0530

    Create influxsonarloop.groovy

---
+++ vars/influxsonarloop.groovy
@@ -0,0 +1,28 @@
+import groovy.json.*
+
+@NonCPS
+create(){
+	def jsonSlurper = new JsonSlurper()
+	def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"),"UTF-8"))
+	def resultJson = jsonSlurper.parse(reader)
+  	print (resultJson)
+  	def size=resultJson.Sonar.Metrics.component.measures.size
+  	print(size)
+  	for(int i=0;i<size;i++){
+		
+    		print(i)
+    	  	String t0=resultJson.Sonar.Metrics.component.measures[i].metric
+	  	String metric=t0.replaceAll("\\[", "").replaceAll("\\]","");
+	  	String t3=resultJson.Sonar.Metrics.component.measures[i].value
+		float value=Float.parseFloat(t3);
+		print (metric)
+          	print (value)
+	}
+
+  sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'SONARDATAnew,Metric=${metric} Value=${value} 1593159883918987231'"""
+}
+
+def call()
+{
+	create()
+}
