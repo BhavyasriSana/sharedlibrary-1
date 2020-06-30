1a166043f1a8dd855a0a64cd71808d8ac1c15158
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-06-30 17:15:50 +0530

    Update gitInflux.groovy

--- vars/gitInflux.groovy
+++ vars/gitInflux.groovy
@@ -17,8 +17,8 @@ def commitscount = resultJson2.size()
 	/*sh """curl -i -XPOST "http://18.222.223.64:8086/write?db=mydb" --data-binary 'GIT,Metric=GitRepoCount Value=${repocount} 1593515953791341888
 	Git,Metric=GitCommitsCountInRepo Value=${commitscount} 1593515953791341888'"""*/
 	
-	sh "curl -i -XPOST 'http://18.222.223.64:8086/write?db=mydb' --data-binary 'GIT,mytag=3 myfield=89 1463689153000000000
-mymeas,mytag=2 myfield=34 1463689152000000000'"
+	sh """curl -i -XPOST 'http://18.222.223.64:8086/write?db=mydb' --data-binary 'GIT,mytag=3 myfield=89 1463689153000000000
+mymeas,mytag=2 myfield=34 1463689152000000000'"""
 
 	
 }
